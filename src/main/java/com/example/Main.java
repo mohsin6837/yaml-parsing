e91332b01f669c6887d11b722649782508bdd9af
Mohsin <mohsinmehboob2@gmail.com>
2024-02-24 19:56:25 +0530

    Added files

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.example.entity.Customer;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.inspector.TagInspector;
+
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        LoaderOptions loaderOptions=new LoaderOptions();
+        TagInspector tagInspector=tag -> tag.getClassName().equals(Customer.class.getName());
+        loaderOptions.setTagInspector(tagInspector);
+        Yaml yaml = new Yaml(new Constructor(Customer.class,loaderOptions));
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("customer.yaml");
+        Customer customer = yaml.load(inputStream);
+        System.out.println(customer.getLastName());
+    }
+}
+
---
+++ src/main/java/com/example/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.example.entity;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    // getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
